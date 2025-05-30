7a29c108ba2fe3d7d47fc5852baca114f59c5da0
ahmed-at-github <ahmedhussainnow@gmail.com>
2025-05-30 18:43:40 +0600

    depl

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/number-format-converter/', // important for GitHub Pages,
+  base: '/react_currency_converter/', // important for GitHub Pages,
   plugins: [react()],
 })
